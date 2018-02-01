45bf8f6a7930a6d4913ae7d3a4b52e838c5b1bfa
Kacper Stroisz <kacperstroisz@wp.pl>
2018-02-01 15:44:18 +0100

    Add .html and .js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+function getTriangleArea(a,h) {
+
+
+	
+
+	if (a > 0 &&  h > 0) {
+		var wynik = a*h/2;
+		return wynik;
+
+}		return "Nieprawidłowe dane";
+
+	
+}		
+
+
+console.log(getTriangleArea(10, 6))
+
+
+console.log(getTriangleArea(10, 15))
+
+
+console.log(getTriangleArea(12, 22))
